c36f02b492c9dfcbab0376910bb1728cbbb28bf9
唐雪 <11183504@qq.com>
2023-04-17 21:05:58 +0800

    build: 降低 tailwindcss 版本， webstrom 才会有智能提醒

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,5 @@ import '@/src/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  console.log('pageProps', Component)
   return <Component {...pageProps} />
 }
