f8ef0d68b8e962deac731757d187e0dfacd7fd85
唐雪 <11183504@qq.com>
2023-04-17 20:56:29 +0800

    feat: 调整文件目录

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import '@/styles/globals.css'
+import '@/src/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+  console.log('pageProps', Component)
   return <Component {...pageProps} />
 }
